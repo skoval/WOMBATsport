0e7966342edde3bec66a5c301ba11433bf23faef
skoval <s.a.kovalchik@gmail.com>
2017-05-29 11:10:47 +1000

    Update to diff_stats

--- R/diff_stats.R
+++ R/diff_stats.R
@@ -1,15 +1,16 @@
 #' Statistic Differences of ATP Matches
 #'
-#' This dataset contains difference statistics for 2016 ATP matches. The variables of the dataset are:
+#' This dataset contains difference statistics for 2016 ATP matches. Each differential is based on the average of the previous 30 matches up to but not including the current match. The variables of the dataset are:
 #'
 #' \itemize{
+#'	 \item matchid. A character identifying match
 #'   \item tourney_name. A character tournament name
 #'   \item surface. A character description of the court surface (Carpet, Clay, Grass, or Hard)
-#'   \item tourney_level. A character indicating the tournament level
+#'   \item player1. A character of the name of player 1
+#'   \item player2. A character of the name of player 2
 #'   \item tourney_date. A Date object of the tournament date
-#'   \item winner_name. A character of the winner's name
-#'   \item loser_name. A character of the loser's name
-#'   \item tourney_date. A Date object of the tournament date
+#'   \item round. A factor of the round
+#'   \item winner. An integer indicating whether player1 was the winner = 1 or loser = 0 of the match
 #'   \item diff_ace_rate. A numeric that is the difference in rate of aces between player 1 and player2.
 #'   \item diff_df_rate. A numeric that is the difference in rate of double faults between player 1 and player2.
 #'   \item diff_first_serve_in_p. A numeric that is the difference in first serve proportion in between player 1 and player2.
@@ -22,9 +23,8 @@
 #'   \item diff_total_won_p. A numeric that is the difference in proportion of total points won between player 1 and player2.
 #'   \item diff_bp. A numeric that is the difference in break points won between player 1 and player2.
 #'   \item diff_bp_opp. A numeric that is the difference in break point opportunities between player 1 and player2.
-#'   \item winner. An integer indicating whether player1 was the winner = 1 or loser = 0 of the match.
 #'}
 #'
-#' @format A data frame with 2,852 rows and 19 variables
+#' @format A data frame with 1,502 rows and 20 variables
 #' @name diff_stats
 NULL
